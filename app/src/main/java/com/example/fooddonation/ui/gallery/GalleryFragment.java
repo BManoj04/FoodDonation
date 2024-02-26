6df9ee0910ab645bcf023db92762ea2113a66ebe
BManoj04 <bmanoj2364@gmail.com>
2024-02-26 22:50:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fooddonation/ui/gallery/GalleryFragment.java
@@ -0,0 +1,28 @@
+package com.example.fooddonation.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.fooddonation.R;
+import com.example.fooddonation.databinding.FragmentGalleryBinding;
+
+public class GalleryFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        return root;
+    }
+
+
+}
