358a5f2d2391d5e219a3bab3de8faf53aad7dbd4
BManoj04 <bmanoj2364@gmail.com>
2024-03-16 15:15:02 +0530

    90% core functionality completed

---
+++ app/src/main/java/com/example/fooddonation/Utils.java
@@ -0,0 +1,7 @@
+package com.example.fooddonation;
+public class Utils {
+    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
+
+    public static final  String EMAIL = "bmanoj2364@gmail.com";
+    public static final String PASSWORD = "sqmt nhlh tobv ybqc";
+}
